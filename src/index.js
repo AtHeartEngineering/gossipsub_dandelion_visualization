c8ec1eec3a03e57ddc6a24471209acf4a79597d3
AtHeartEngineer <1675654+AtHeartEngineer@users.noreply.github.com>
2022-04-21 10:49:47 -0400

    title update
    title update minor

--- src/index.js
+++ src/index.js
@@ -3,5 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+document.title = "Gossipsub++";
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
